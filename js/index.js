4a78d2a2df4e6e9f461da7777c9fc446ced1e8bf
kiranpariyar <kirannpariyar@gmail.com>
2016-01-01 11:34:10 +0545

    added header hide function

--- js/index.js
+++ js/index.js
@@ -1,12 +1,87 @@
-var creativeImageWrapper = document.getElementsByClassName('creativeimage-wrapper')[0];
-var style = window.getComputedStyle(creativeImageWrapper);
-var height = style.getPropertyValue('height');
-// console.log("height :",height);
-
-// var viewportHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 var viewportHeight = window.innerHeight;
-var sliderImage = document.getElementsByClassName('slider-image');
-var image = sliderImage[0].children;
-console.log('image :',image);
-console.log('viewportHeight :',viewportHeight);
-// image[0].style.height = viewportHeight + 'px';
+var body = document.getElementsByTagName("body")[0];
+var isHiding = false;
+var headerWrapper = document.getElementsByClassName('header-wrapper')[0];
+var style;
+var hideTop;
+var headerheight;
+var positionTop  
+
+
+body.onscroll = function(){
+	countUp();
+	calculateVerticalScroll();
+}
+
+
+function countUp() {
+
+	var infoValue = document.getElementsByClassName("info-values");
+	var infoContent = document.getElementsByClassName("info-values")[0];
+	var isInViewport = elementInViewport(infoContent);
+	// console.log(isInViewport);
+	var endValue = [79000,754,4000,1000];
+	var count = new Counter();
+	if(infoValue[0].innerHTML == 0 && infoValue[1].innerHTML == 0
+		&& infoValue[2].innerHTML == 0 && infoValue[3].innerHTML == 0 && isInViewport){
+		var startValue;
+		var targetElement;
+		var duration;
+
+		for (var i = 0; i < infoValue.length; i++) {
+			startValue = infoValue[i].innerHTML;
+			targetElement = infoValue[i];
+			duration = 2000;
+			count.counterUp(targetElement,startValue,endValue[i],duration);
+		};
+	}	
+}
+
+
+function elementInViewport(el) {
+  var top = el.offsetTop;
+  var left = el.offsetLeft;
+  var width = el.offsetWidth;
+  var height = el.offsetHeight;
+
+  while(el.offsetParent) {
+    el = el.offsetParent;
+    top += el.offsetTop;
+    left += el.offsetLeft;
+  }
+
+  return (
+    top >= window.pageYOffset &&
+    left >= window.pageXOffset &&
+    (top + height) <= (window.pageYOffset + window.innerHeight) &&
+    (left + width) <= (window.pageXOffset + window.innerWidth)
+  );
+} 
+
+function calculateVerticalScroll(){
+	var scrollDownY = body.scrollTop;
+	console.log('scrollY :',scrollDownY);
+	console.log('viewportHeight :',viewportHeight);
+	if(scrollDownY >= viewportHeight && isHiding == false){		
+		hideHeader();		
+	}else if(viewportHeight > scrollDownY && isHiding == true){
+		showHeader();
+	}
+}
+
+function hideHeader(){
+	console.log('hideHeader');
+	style = window.getComputedStyle(headerWrapper);
+	positionTop = style.getPropertyValue('top');
+	headerheight = headerWrapper.clientHeight;
+	hideTop = - parseInt(positionTop) - parseInt(headerheight);
+	headerWrapper.style.top =  hideTop + 'px';
+	isHiding = true;
+}
+
+
+function showHeader(){
+	console.log('showHeader');
+	headerWrapper.style.top = hideTop * (-1) - headerheight + 'px';
+	isHiding = false;
+}
--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-var sliderLong = document.getElementsByClassName("slider-long")[0];
+var sliderLong = document.getElementsByClassName('slider-long')[0];
 var sliderImage = sliderLong.children[0]; 
 var slides = sliderLong.children;
 var sliding = false;
@@ -7,47 +7,45 @@ var active = 1;
 var imgWidth;
 var animator = new Animator(sliderLong);
 
-function calculateImageWidth(currentImage){
-	var imageElement = document.getElementsByClassName("slider-image")[currentImage-1];
+function calculateImageWidth(currentImage) {
+	var imageElement = document.getElementsByClassName('slider-image')[currentImage-1];
 	var style = window.getComputedStyle(imageElement);
-	var imageWidth = parseInt(style.getPropertyValue("width"));
+	var imageWidth = parseInt(style.getPropertyValue('width'));
 	return imageWidth;
 }
 
 function slide() {
 	if(animator.animation==false){
 		imgWidth = calculateImageWidth(active);
-		// console.log(imgWidth);
 		active = active == slides.length ? active = 1 : ++active;
 		var ml = (imgWidth * (active-1) * -1);
-		animator.animate("margin-left",ml,1000);
+		animator.animate('margin-left',ml,1000);
 	}
 	
 }
 
-// var intervalId=setInterval(slide, 3000);
-var intervalId;
+var intervalId=setInterval(slide, 3000);
 
-function next(){
-	if(active!=slides.length){
+function next() {
+	if(active != slides.length){
 		imgWidth = calculateImageWidth(active);
 		animator.finish();	
 		window.clearInterval(intervalId);
 		active++;
 		var ml = (imgWidth * (active-1) * -1);
-		animator.animate("margin-left",ml,1000);
+		animator.animate('margin-left',ml,1000);
 		intervalId=setInterval(slide, 3000);	
 	}	
 }
 
-function previous(){
-	if(active!=1){
+function previous() {
+	if(active != 1){
 		imgWidth = calculateImageWidth(active);
 		animator.finish();
 		window.clearInterval(intervalId);
 		active--;
 		var ml = (imgWidth * (active-1) * -1);
-		animator.animate("margin-left",ml,1000);
+		animator.animate('margin-left',ml,1000);
 		intervalId=setInterval(slide, 3000);
 	}	
 }
